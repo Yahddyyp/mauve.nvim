b80a31ae0ce2aeee0776734b3180bc87e08f5d8e
Yahddyyp <Yahddyyp@gmail.com>
2026-05-16 20:10:09 +0530

    Fixed up fidget

--- lua/mauve-nvim/integrations/fidget.lua
+++ lua/mauve-nvim/integrations/fidget.lua
@@ -1,12 +1,12 @@
 return function(c)
-    local highlights = {
-        FidgetTask = { fg = c.subtext, bg = "NONE" },
-        FidgetTitle = { fg = c.mauve, bg = "NONE", bold = true },
-        FidgetSpinner = { fg = c.blue, bg = "NONE" },
-        FidgetBorder = { fg = c.surface0, bg = c.mantle },
-        FidgetGroup = { fg = c.overlay2, bg = "NONE" },
-    }
-    for group, opts in pairs(highlights) do
-        vim.api.nvim_set_hl(0, group, opts)
-    end
+	local highlights = {
+		FidgetTask = { fg = c.subtext, bg = "NONE" },
+		FidgetTitle = { fg = c.mauve, bg = "NONE", bold = true },
+		FidgetSpinner = { fg = c.blue, bg = "NONE" },
+		FidgetBorder = { fg = c.surface0, bg = c.mantle },
+		FidgetGroup = { fg = c.overlay2, bg = "NONE" },
+	}
+	for group, opts in pairs(highlights) do
+		vim.api.nvim_set_hl(0, group, opts)
+	end
 end
