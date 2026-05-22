3d0d797f18a9ea3e8b6088d817047c4b591b717b
Yahddyyp <Yahddyyp@gmail.com>
2026-05-22 13:12:52 +0530

    Made blinkcmp not use bg

--- lua/mauve/integrations/blinkcmp.lua
+++ lua/mauve/integrations/blinkcmp.lua
@@ -1,20 +1,20 @@
 return function(c)
-    local highlights = {
-        BlinkCmpMenu = { bg = c.mantle, fg = c.fg },
-        BlinkCmpMenuBorder = { bg = c.mantle, fg = c.mauve },
-        BlinkCmpMenuSelection = { bg = c.surface0, fg = c.sapphire },
-        BlinkCmpScrollBarBorder = { bg = c.mantle },
-        BlinkCmpScrollBarThumb = { bg = c.mauve },
-        BlinkCmpLabel = { fg = c.fg },
-        BlinkCmpLabelDeprecated = { fg = c.overlay0, strikethrough = true },
-        BlinkCmpLabelMatch = { fg = c.mauve, bold = true },
-        BlinkCmpKind = { fg = c.mauve },
-        BlinkCmpKindCopilot = { fg = c.mauve },
-        BlinkCmpSource = { fg = c.sapphire },
-        BlinkCmpDoc = { bg = c.bg },
-        BlinkCmpDocBorder = { bg = c.bg, fg = c.sapphire },
-    }
-    for group, opts in pairs(highlights) do
-        vim.api.nvim_set_hl(0, group, opts)
-    end
+	local highlights = {
+		BlinkCmpMenu = { bg = "NONE", fg = c.fg },
+		BlinkCmpMenuBorder = { bg = "NONE", fg = c.mauve },
+		BlinkCmpMenuSelection = { bg = c.surface0, fg = c.sapphire },
+		BlinkCmpScrollBarBorder = { bg = "NONE" },
+		BlinkCmpScrollBarThumb = { bg = c.mauve },
+		BlinkCmpLabel = { fg = c.fg },
+		BlinkCmpLabelDeprecated = { fg = c.overlay0, strikethrough = true },
+		BlinkCmpLabelMatch = { fg = c.mauve, bold = true },
+		BlinkCmpKind = { fg = c.mauve },
+		BlinkCmpKindCopilot = { fg = c.mauve },
+		BlinkCmpSource = { fg = c.sapphire },
+		BlinkCmpDoc = { bg = "NONE" },
+		BlinkCmpDocBorder = { bg = "NONE", fg = c.sapphire },
+	}
+	for group, opts in pairs(highlights) do
+		vim.api.nvim_set_hl(0, group, opts)
+	end
 end
