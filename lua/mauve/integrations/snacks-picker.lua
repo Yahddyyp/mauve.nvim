df4146585c8de9128f111a66463d4197888fa74c
Yahddyyp <Yahddyyp@gmail.com>
2026-07-21 20:44:51 +0530

    fix(ui): Finally made snacks-picker look better and clean

--- lua/mauve/integrations/snacks-picker.lua
+++ lua/mauve/integrations/snacks-picker.lua
@@ -1,24 +1,40 @@
 return function(c)
 	local highlights = {
-		SnacksPicker = { bg = c.mantle, fg = c.fg },
-		SnacksPickerBorder = { bg = c.mantle, fg = c.mauve },
-		SnacksPickerTitle = { fg = c.sapphire },
-		SnacksPickerInput = { bg = c.mantle, fg = c.fg },
-		SnacksPickerInputBorder = { bg = c.mantle, fg = c.mauve },
-		SnacksPickerInputTitle = { fg = c.sapphire, bg = c.mantle },
-		SnacksPickerList = { bg = c.mantle, fg = c.fg },
-		SnacksPickerListBorder = { bg = c.mantle, fg = c.mauve },
-		SnacksPickerListTitle = { fg = c.sapphire },
-		SnacksPickerListCursorLine = { bg = c.surface0 },
-		SnacksPickerPreview = { bg = c.mantle, fg = c.fg },
-		SnacksPickerPreviewBorder = { bg = c.mantle, fg = c.sapphire },
-		SnacksPickerPreviewTitle = { fg = c.sapphire },
+		SnacksNormal = { link = "Normal" },
+		SnacksWinBar = { link = "Title" },
+		SnacksBackdrop = { link = "FloatShadow" },
+		SnacksNormalNC = { link = "NormalFloat" },
+		SnacksWinBarNC = { link = "SnacksWinBar" },
+
+		SnacksPicker = { fg = c.fg },
+		SnacksPickerBorder = { fg = c.mauve },
+		SnacksPickerInputBorder = { fg = c.mauve },
+		SnacksPickerInput = { fg = c.fg },
+		SnacksPickerPrompt = { fg = c.sapphire },
+		SnacksPickerMatch = { fg = c.blue },
+
+		SnacksPickerSelected = {
+			fg = c.mauve,
+			bold = true,
+		},
+
+		SnacksPickerList = { fg = c.fg },
+		SnacksPickerListBorder = { fg = c.mauve },
+		SnacksPickerListCursorLine = { fg = c.mauve, bg = c.surface1, bold = true },
+		SnacksPickerListTitle = { fg = c.mauve },
+
+		SnacksPickerPreview = { fg = c.fg },
+		SnacksPickerPreviewBorder = { fg = c.sapphire },
+		SnacksPickerPreviewTitle = { fg = c.green },
+
+		SnacksPickerTitle = { fg = c.mauve },
+		SnacksPickerInputTitle = { fg = c.blue },
+
 		SnacksPickerDir = { fg = c.overlay0 },
-		SnacksPickerRow = { fg = c.sapphire },
+		SnacksPickerRow = { fg = c.mauve },
 		SnacksPickerCol = { fg = c.overlay0 },
 		SnacksPickerFileName = { fg = c.fg },
-		SnacksPickerMatch = { fg = c.mauve, bold = true },
-		SnacksPickerSelected = { fg = c.mauve },
+
 		SnacksPickerGitAdded = { fg = c.green },
 		SnacksPickerGitDeleted = { fg = c.red },
 		SnacksPickerGitModified = { fg = c.yellow },
@@ -28,8 +44,8 @@ return function(c)
 		SnacksPickerGitScope = { fg = c.sapphire },
 		SnacksPickerGitBranch = { fg = c.mauve },
 		SnacksPickerGitCommit = { fg = c.lavender },
-		SnacksPickerPrompt = { fg = c.sapphire },
 	}
+
 	for group, opts in pairs(highlights) do
 		vim.api.nvim_set_hl(0, group, opts)
 	end
